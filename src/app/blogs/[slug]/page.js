319a7213f58bfa0e655001091c59086cc798eed0
vatiza <rakibur.mac@gmail.com>
2024-09-01 12:13:43 +0600

    navbar search field re design

--- src/app/blogs/[slug]/page.js
+++ src/app/blogs/[slug]/page.js
@@ -18,35 +18,48 @@ const detailsPage = async ({ params }) => {
   } = blog;
 
   return (
-    <div className="mt-20 flex justify-center ">
-      <div className="bg-base-100  shadow-xl">
-        <figure>
-          <Image width={400} height={400} src={img} alt={title} />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title font-bold text-2xl">{title}</h2>
-          <p>{description}</p>
-        </div>
-        <div className="divider"></div>
-        <div className="flex items-center gap-2 mb-5">
-          <div className="avatar">
-            <div className="w-12  rounded-full">
-              <Image
-                width={20}
-                height={20}
-                src={bloggerImg}
-                alt={bloggerFirstName}
-              ></Image>
-            </div>
-          </div>
+    <div className="mockup-browser bg-base-300 border mt-32">
+      <div className="mockup-browser-toolbar">
+        <div className="input">{title}</div>
+      </div>
+      <div
+        className="bg-base-200 
+       p-1 lg:p-4"
+      >
+        <div className=" flex justify-center ">
           <div>
-            <h1 className="font-semibold">
-              {bloggerFirstName} {bloggerLastName}
-            </h1>
-            <a className="hover:text-warning" href={`mailto:${emailAddresses}`}>
-              {emailAddresses}
-            </a>
-            <p className="">{moment({ postdate }).format("LLL")}</p>
+            <figure>
+              <Image width={400} height={400} src={img} alt={title} />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title font-bold text-2xl">{title}</h2>
+              <p>{description}</p>
+            </div>
+            <div className="divider"></div>
+            <div className="flex items-center gap-2 mb-5">
+              <div className="avatar">
+                <div className="w-12  rounded-full">
+                  <Image
+                    width={20}
+                    height={20}
+                    src={bloggerImg}
+                    alt={bloggerFirstName}
+                  ></Image>
+                </div>
+              </div>
+              <div>
+                <h1 className="font-semibold">
+                  {bloggerFirstName} {bloggerLastName}
+                </h1>
+                <a
+                  className="hover:text-warning"
+                  href={`mailto:${emailAddresses}`}
+                >
+                  {emailAddresses}
+                </a>
+                <p className="">{moment({ postdate }).format("LLL")}</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- src/components/blogs/navbar/BlogNavbar.js
+++ src/components/blogs/navbar/BlogNavbar.js
@@ -52,7 +52,7 @@ const BlogNavbar = () => {
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 16 16"
           fill="currentColor"
-          className="h-4 w-4 opacity-70"
+          className="h-4 w-4 opacity-70 "
         >
           <path
             fillRule="evenodd"
