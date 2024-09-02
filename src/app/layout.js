eb883fffc1860cb79d6c4fc3c3a3ac6ad5a5bbf7
vatiza <rakibur.mac@gmail.com>
2024-09-02 16:11:48 +0600

    vercel Analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,14 +13,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider
-    publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
-  >
-    <html lang="en">
-      <body className={inter.className}>
-        <div className="lg:mx-20 h-screen max-h-svh">{children}</div>
-        <Analytics />
-      </body>
-    </html>
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
+      <html lang="en">
+        <body className={inter.className}>
+          <div className="lg:mx-20 h-screen max-h-svh">
+            {children}
+            <Analytics />
+          </div>
+        </body>
+      </html>
     </ClerkProvider>
   );
 }
