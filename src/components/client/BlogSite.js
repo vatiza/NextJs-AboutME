3845516396be9541c7b54ff9922df3fecb17b0b8
vatiza <rakibur.mac@gmail.com>
2024-09-21 20:52:49 +0600

    project add and fix style

---
+++ src/components/client/BlogSite.js
@@ -0,0 +1,17 @@
+"use client";
+
+const BlogSite = () => {
+  return (
+    <div className=" gap-8">
+      <div>
+        <a className="btn btn-warning btn-sm" href="https://www.rakibs.live/">
+          Live View
+        </a>
+      </div>
+     
+    
+    </div>
+  );
+};
+
+export default BlogSite;
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -2,7 +2,7 @@ import rafsanMock from "../../assets/img/rafsanmock.png";
 import blogsImg from "../../assets/img/blogs.png";
 import Image from "next/image";
 import RafsanTravels from "../client/RafsanTravels";
-import Links from "../../links/Links";
+import BlogSite from "../client/BlogSite";
 
 const Projects = () => {
   return (
@@ -35,20 +35,21 @@ const Projects = () => {
         </div>
       </div>
       {/* TODO: --------------Project 2----------------- */}
-      <div className="  px-4 sm:px-6 lg:px-8">
-        <div className="flex">
-          <div className=" rounded-lg  mb-4">
-            <Image
-              priority={false}
-              src={blogsImg}
-              className="object-cover transition-transform duration-300 hover:scale-105 "
-              alt="rakib blogs"
-            ></Image>
+      <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 mt-5">
+        <div className="flex flex-col md:flex-row -mx-4">
+          <div className="md:flex-col-reverse px-4">
+            <div className=" rounded-lg  mb-4">
+              <Image
+                priority={false}
+                src={blogsImg}
+                className="object-cover transition-transform duration-300 hover:scale-105 "
+                alt="rakib blogs"
+              ></Image>
+            </div>
           </div>
-
-          <div className=" px-4">
+          <div className="px-4">
             <h2 className="text-2xl font-bold text-gray-800 dark:text-white mb-2">
-              Blog<span className="text-warning">&apos;s</span> Site
+              Blogs
             </h2>
             <p className="text-gray-600 dark:text-gray-300 text-sm mb-4">
               This is where I share my personal experiences and thoughts on
@@ -56,7 +57,7 @@ const Projects = () => {
               coding, and development. Explore my blogs to learn more about my
               journey.
             </p>
-            <Links />
+            <BlogSite />
           </div>
         </div>
       </div>
